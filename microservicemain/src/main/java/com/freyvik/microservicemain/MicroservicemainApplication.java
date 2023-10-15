7947c60c660c4cc29afee99cd1b1da87c4646b5d
FreyVik <yonkigamer96@gmail.com>
2023-10-15 17:19:04 +0200

    main service

---
+++ microservicemain/src/main/java/com/freyvik/microservicemain/MicroservicemainApplication.java
@@ -0,0 +1,13 @@
+package com.freyvik.microservicemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicemainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicemainApplication.class, args);
+	}
+
+}
---
+++ microservicemain/src/test/java/com/freyvik/microservicemain/MicroservicemainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freyvik.microservicemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicemainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
